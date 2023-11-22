0387702a489613af605cc6d14bcb6d3d9186f6c2
shj9206 <shj9206@gmail.com>
2023-11-22 11:48:33 +0900

    store 기능 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import store from "@/store";
+import { Provider } from "react-redux";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -18,7 +19,7 @@ import EditContact, { action as editAction } from "@/views/sample/edit";
 import { action as destroyAction } from "@/views/sample/destroy";
 import Index from "@/views/sample/index";
 import ErrorPage from "@/views/sample/error-page";
-
+import StoreSampe from "@/views/storeSample/storeSample";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -49,12 +50,13 @@ const router = createBrowserRouter([
         action: destroyAction,
         errorElement: <div>Oops! There was an error.</div>,
       },
+      { path: "/views/sample/store", element: <StoreSampe /> },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </Provider>
 );
