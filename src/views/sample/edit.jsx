e3f23a7f3dbe48575a63eb322a6ab73bd97e28a6
shj9206 <shj9206@gmail.com>
2023-11-22 17:08:37 +0900

    save action 경로 수정

--- src/views/sample/edit.jsx
+++ src/views/sample/edit.jsx
@@ -9,7 +9,7 @@ export const action =
     const updates = Object.fromEntries(formData);
     await updateContact(params.contactId, updates);
     queryClient.invalidateQueries({ queryKey: ["contacts"] });
-    return redirect(`/contacts/${params.contactId}`);
+    return redirect(`/views/sample/contacts/${params.contactId}`);
   };
 
 export default function Edit() {
